283f65aadc17ddd8b3b628cad6b7a299b3d4d97a
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-25 20:44:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect {|e| e.upcase + "!"}
+  array.collect {|e| e.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
