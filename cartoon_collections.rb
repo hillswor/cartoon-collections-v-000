813e094105e833b1a8b2331732ba12ed0a56ea05
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-25 20:36:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,10 @@ def roll_call_dwarves(array)
   puts "#{dwarves_list}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect {|e| e + "!"}
+  end
+  array
 end
 
 def long_planeteer_calls# code an argument here
